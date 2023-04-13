f6c25205102e290f5ac072134a1b73dbc4b0de2f
sorandada <sora.nkz.ddd@gmail.com>
2023-04-13 10:04:42 +0900

    4/12_変更

--- karo/models.py
+++ karo/models.py
@@ -1,28 +1,23 @@
 from django.db import models
 from django.utils import timezone
-
-
-
-
 # Create your models here.
 
 CHOICE = (("secondary", "NIGHT"), ("primary", "DAYTIME"), ("danger", "MORNING"))
 
-def ten():
-    pkpk=KaroModel.objects.get(pk=84).goleKcol
-    return pkpk
-
-
 class KaroModel(models.Model):
+    #食べたもの
     eat = models.CharField(max_length=20)
+    #食べたもののカロリー
     kcol = models.IntegerField(null=True, blank=True, default=0)
+    #食べた時間帯（朝、昼、夜）
     daycolor = models.CharField(
         max_length=10,
         choices = CHOICE,
         default = "secondary"
         )
-
+    #食べた時間
     date = models.DateTimeField(default=timezone.now())
+    #目標カロリーの設定
     goleKcol = models.IntegerField(default=0)
 
     def __str__(self):
--- karo/views.py
+++ karo/views.py
@@ -5,52 +5,36 @@ from django.urls import reverse_lazy
 from django.shortcuts import redirect
 from django.db.models import Count, Avg, Sum, Max, Min
 
-# Create your views here.
+import datetime
 
-#class KaroList(ListView):
-    #template_name = "list.html"
-    #model = KaroModel
+# Create your views here.
 
 def KaroList(request):
     object_list=KaroModel.objects.all()
-
     kcolsum=object_list.aggregate(Sum("kcol"))
     golekcolmax=object_list.aggregate(Max("goleKcol"))
-    golekcolmin=object_list.aggregate(Min("goleKcol"))
-
-
 
     #####################目標カロリーの表示##############################
-    #最後）のレコードを取得
     last_order = KaroModel.objects.order_by("id").last()
-    #print(last_order)
     golekcolcount=object_list.aggregate(Count("goleKcol"))
     golekcolcount=golekcolcount["goleKcol__count"]
 
     l=[]
     for i in object_list:
         l.append(i.id)
-    #print(l)
     
     gole=0
     if KaroModel!=None and len(l)>1:
-        #print("ok")
         gole=last_order.goleKcol
         print(gole)
         print(golekcolmax["goleKcol__max"])
         print(KaroModel.objects.get(pk=l[-2]).goleKcol)
         if (gole == 0) or (gole < golekcolmax["goleKcol__max"]):
-            #print(KaroModel.objects.get(pk=l[-2]).goleKcol)
-            if (KaroModel.objects.get(pk=l[-2]).goleKcol == 0):
-                #l = sorted(l, reverse=True)
-                #print(l) 
-                
+            if (KaroModel.objects.get(pk=l[-2]).goleKcol == 0):        
                 for i in object_list:
                     if 0 != i.goleKcol:
                         gole = i.goleKcol
                         print(gole)
-                #l = sorted(l, reverse=False)
-                #print(l) 
                 print(gole)  
             elif (gole != KaroModel.objects.get(pk=l[-2]).goleKcol):
                 
@@ -58,50 +42,41 @@ def KaroList(request):
                     for i in object_list:
                         if 0 != i.goleKcol:
                             gole = i.goleKcol
-                
                 gole = gole
-                        
 
             else:
                 gole=KaroModel.objects.get(pk=l[-2]).goleKcol
-                #print(gole, "sss")
         else:
             gole=last_order.goleKcol
-            #print(gole, "aaa")
         
     else:
-        #print("no")
-        #print(l)
         gole=0
-        #print(gole)
         for i in object_list:
             gole = i.goleKcol 
-            #print(gole)
     ###################### ここまで　############################
 
+    #摂取カロリーの合計
     if kcolsum["kcol__sum"] == None:
         kcolsum["kcol__sum"]=0
         lastkcal = gole-kcolsum["kcol__sum"]
     else:
         lastkcal = gole-kcolsum["kcol__sum"]
 
-    import datetime
-    dt_now = datetime.datetime.now()
-    #print(dt_now)
 
-    
+    dt_now = datetime.datetime.now()
 
     return render(request, "list.html", {"object_list": object_list, "kcolsum":kcolsum, "golekcolmax":golekcolmax,
-    "golekcolmin":golekcolmin,"last_order":last_order, "gole":gole, "dt_now":dt_now,"lastkcal":lastkcal, "golekcolcount":golekcolcount})
+    "last_order":last_order, "gole":gole, "dt_now":dt_now,"lastkcal":lastkcal, "golekcolcount":golekcolcount})
 
     
 
 
-
+#詳細
 class KaroDetail(DetailView):
     template_name="detail.html"
     model = KaroModel
 
+#作成
 class KaroCreate(CreateView):
     template_name="create.html"
     model = KaroModel
@@ -109,22 +84,20 @@ class KaroCreate(CreateView):
     fields = ("eat", "kcol", "daycolor", "date", "goleKcol", )
     success_url = reverse_lazy("list")
 
+#削除
 class KaroDelete(DeleteView):
     template_name="delete.html"
     model = KaroModel
     success_url = reverse_lazy("list")
 
+#更新
 class KaroUpdate(UpdateView):
     template_name="update.html"
     model = KaroModel
     fields = ("eat", "kcol", "daycolor")
     success_url = reverse_lazy("list")
 
-
-
-
-
-
+#全削除
 def fulldelete(request):
     template_name = "fulldelete.html"
     if request.POST:
